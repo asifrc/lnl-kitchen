9a0aa2e14899e00cded7a5e6697ae7614832806f
Asif R Choudhury <choudhury.asif@gmail.com>
2016-09-13 11:58:12 +0530

    Added bundler gem to base image

--- base_image/cookbooks/base/recipes/default.rb
+++ base_image/cookbooks/base/recipes/default.rb
@@ -3,3 +3,5 @@ package "git"
 package "ruby" do
   version "2.0.0.598-25.el7_1"
 end
+
+gem_package "bundler"
--- base_image/test/integration/default/serverspec/default_spec.rb
+++ base_image/test/integration/default/serverspec/default_spec.rb
@@ -7,3 +7,7 @@ end
 describe package("ruby") do
   it { should be_installed.with_version("2.0.0.598-25.el7_1") }
 end
+
+describe package("bundler") do
+  it { should be_installed.by("gem") }
+end
